501498d344a827b0aefba1849309391541eb0d40
freeeakn <1rycont@gmail.com>
2024-05-26 00:56:01 +0700

    fix auth service

--- frontend/src/services/auth/auth.interface.ts
+++ frontend/src/services/auth/auth.interface.ts
@@ -1,5 +1,9 @@
 // import { IUser } from '../users/users.interface'
 
+export interface IToken {
+  auth_token: string
+}
+
 export interface IAuthResponse {
   id: number | string
   email: string
--- frontend/src/services/auth/auth.service.ts
+++ frontend/src/services/auth/auth.service.ts
@@ -1,11 +1,11 @@
-import { IAuthResponse, ISignInRequest } from './auth.interface'
+import { ISignInRequest, IToken } from './auth.interface'
 import { instance } from '../../api/api.interceptor'
 
 const PATH = 'auth'
 
 export const AuthService = {
   async login(userData: ISignInRequest) {
-    const response = await instance.post<IAuthResponse>(
+    const response = await instance.post<IToken>(
       `/${PATH}/token/login/`,
       userData
     )
