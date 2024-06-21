17e175721c6341c638f78e3720ac9d998cea180c
skyfxllen <anton.tatarchenko3@gmail.com>
2024-06-22 01:17:34 +0700

    Table

--- frontend/src/components/layout/UserDashboard/UserDashboard.tsx
+++ frontend/src/components/layout/UserDashboard/UserDashboard.tsx
@@ -1,15 +1,17 @@
-import { useAuth } from "../../../hooks/useAuth"
-import { useCheckRole } from "../../../hooks/useCheckRole"
-import Accepted from "../../section/User/Accepted/Accepted"
-import NotAccepted from "../../section/User/NotAccepted/NotAccepted"
-import UploadFile from "../../section/User/UploadFile/UploadFile"
+import { useAuth } from '../../../hooks/useAuth'
+import { useCheckRole } from '../../../hooks/useCheckRole'
+import Accepted from '../../section/User/Accepted/Accepted'
+import NotAccepted from '../../section/User/NotAccepted/NotAccepted'
+import UploadFile from '../../section/User/UploadFile/UploadFile'
+import Table from '../Table/Table'
 
 const UserDashboard = () => {
   useAuth()
   useCheckRole('user')
   return (
     <div className="py-20">
-      <UploadFile />
+      <Table />
+      {/* <UploadFile /> */}
       <NotAccepted />
       <Accepted />
     </div>
